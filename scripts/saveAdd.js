7c306337b935468a05fd8de75fb8a8ab895eaf74
zekariyastariku21-ux <zekariyastariku21@gmail.com>
2026-09-09 22:55:35 +0300

    Update saveAdd.js

--- scripts/saveAdd.js
+++ scripts/saveAdd.js
@@ -10,7 +10,7 @@ let isAdminLoggedIn = false;
 const API_BASE = (location.port && location.port !== '3000') ? `${location.protocol}//${location.hostname}:3000` : '';
 let foods = {
   breakfast: [{
-    image: 'image/foul.jpeg',
+    image: 'image/fouls.jpeg',
     ingridient: 'Freshly baked pita, rich beans, eggs, and a touch of spice.',
     name: 'SPECIAL FOUL',
     price: 750,
